8168ab2398923af3700ee1f834c21d28816ff53c
Tirth <tirthgpatel.27@gmail.com>
2024-12-25 16:49:23 +0530

    feat: Repo setup and initial commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react-refresh/only-export-components */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// export * from './components'
+// export * from './constants'
+// export * from './context'
+// export * from './hooks'
+// export * from './types'
